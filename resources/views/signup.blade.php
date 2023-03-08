ea5c923fb7f4f45462ce94df6b7f4210c4297e7e
monower <sadafahmed720@gmail.com>
2023-03-09 01:23:55 +0600

    worked on signup page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -7,82 +7,8 @@
     <meta charset="utf-8" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
   </head>
   <body>
-{{--     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">GreenHub</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav me-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">About
-                  <span class="visually-hidden">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('login')}}">Login</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      
-
-      <div class="h-100 d-flex align-items-center justify-content-center p-5">
-        <form action="{{route('user.signup')}}" method="POST">
-            @csrf
-            <fieldset>
-              <legend>Signup</legend>
-              <small class="text-muted">Please fill in this form to create account.</small>
-              <div class="form-group py-2">
-                @error('name')
-                  <div>
-                    <p style="color: red">name is required</p>
-                  </div>
-                @enderror
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" name="name">
-              </div>
-              <div class="form-group py-2">
-                @error('address')
-                  <div>
-                    <p style="color: red">address is required</p>
-                  </div>
-                @enderror
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Address" name="address">
-              </div>
-              <div class="form-group py-2">
-                @error('email')
-                  <div>
-                    <p style="color: red">email is required</p>
-                  </div>
-                @enderror
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email">
-              </div>
-              <div class="form-group py-2">
-                @error('password')
-                  <div>
-                    <p style="color: red">password is required</p>
-                  </div>
-                @enderror
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
-              </div>
-              <div class="form-group py-2">
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Retype Password" name="password_confirmation">
-              </div>
-              <button type="submit" class="btn btn-primary">Sign Up</button>
-            </fieldset>
-          </form>
-      </div>
-    </div> --}}
-
-
-
     <div class="container mx-auto flex justify-center pt-[100px]">
       <div class="bg-slate-300 w-[33%] p-5 rounded-2xl">
           <form class="flex flex-col space-y-5" action="{{route('user.signup')}}" method="post">
